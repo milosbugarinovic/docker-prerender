934e3ba90f47451206954b8e065c574cd37eb2f6
Milos Bugarinovic <milos@trucktrack.co>
2016-04-17 11:02:48 +0200

    server changes

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
 var prerender = require('prerender');
 
+// var server = prerender({
+// 	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+// 	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+// 	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+// 	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+//     });
+//
+//
+// server.use(prerender.removeScriptTags());
+//
+// if (process.env.BASIC_AUTH_USERNAME) {
+//     server.use(prerender.basicAuth());
+// }
+//
+// server.use(prerender.inMemoryHtmlCache());
+//
+// //server.use(prerender.blacklist());
+// //server.use(prerender.httpHeaders());
+// // server.use(prerender.s3HtmlCache());
+// // server.use(require('my-plugin'));
+
+// server.start();
+
+
 var server = prerender({
-	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-    });
+	workers: process.env.PRERENDER_NUM_WORKERS,
+	iterations: process.env.PRERENDER_NUM_ITERATIONS
+});
 
 
+server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.basicAuth());
+server.use(prerender.whitelist());
+server.use(prerender.blacklist());
+// server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
-
-if (process.env.BASIC_AUTH_USERNAME) {
-    server.use(prerender.basicAuth());
-}
-
-server.use(prerender.inMemoryHtmlCache());
-
-//server.use(prerender.blacklist());
-//server.use(prerender.httpHeaders());
+server.use(prerender.httpHeaders());
+// server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
-// server.use(require('my-plugin'));
 
 server.start();
